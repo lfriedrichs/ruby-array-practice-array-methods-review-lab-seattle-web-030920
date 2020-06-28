80f900cb1448c0f8ffd5b95290ec3bb7057cca8a
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-06-28 16:30:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,13 +7,17 @@ def array_with_two_elements
 end
 
 def first_element(array)
-  array.first
+  array[0]
 end
 
 def third_element(array)
-  array.third
+  array[2]
 end
 
 def last_element(array)
-  array.last
+  array[-1]
+end
+
+def first_element_with_array_methods(array)
+  array.first
 end
